815a0841ed111e0c700e22234e37a7ab428d3621
kimcchen <kimberlykchen@gmail.com>
2025-01-25 16:52:51 -0600

    add cors

--- server/router.js
+++ server/router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 const askClaudeController = require('./questionGenerator')
 
+app.use(cors({ origin: false }));
+
 app.get('/askClaude', askClaudeController);
 
 app.listen(8080, () => {
