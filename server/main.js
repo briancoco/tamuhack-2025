9ab073458a153200b351e1dd42c49ad65c594131
briancoco <briannguyencoco@gmail.com>
2025-01-25 13:50:47 -0600

    mongodb atlas setup

--- server/main.js
+++ server/main.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const connectDB = require('./db/connect')
 
 app.get('/', (req, res) => {
   res.json("Hello World")
   return
 })
 
-const port = process.env.PORT
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+const start = async () => {
+  const port = process.env.PORT
+  try {
+    await connectDB()
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`)
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+start()
