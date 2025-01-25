c2a71389c6d387122bc2b11a1693c327497020ae
Andrew Mao <maosteram@gmail.com>
2025-01-25 16:03:20 -0600

    Adds link submission functionality

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,18 +1,32 @@
-import React from 'react'
-import { TextField, Button } from '@radix-ui/themes'
-import './home.css'
-
+import React, { useState } from "react";
+import { Button } from "@radix-ui/themes";
+import "./home.css";
 
 const Home = () => {
+  const [youtubeLink, setYoutubeLink] = useState("");
+
+  const handleSubmit = () => {
+    console.log("handling submit", youtubeLink);
+    // route to other page
+    // send youtube link data
+  };
+
   return (
-    <div className='container'>
-      <div className='content'>
-        <h1 className='title'>-MumboJumbo.</h1>
-        <input className='textfield' type="text" placeholder='Enter youtube url'></input>
-        <Button className='searchButton'>Search</Button>
+    <div className="container">
+      <div className="content">
+        <h1 className="title">-MumboJumbo.</h1>
+        <input
+          className="textfield"
+          type="text"
+          placeholder="Enter youtube url"
+          onChange={(event) => setYoutubeLink(event.target.value)}
+        ></input>
+        <Button className="searchButton" onClick={handleSubmit}>
+          Search
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
