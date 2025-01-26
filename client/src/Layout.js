5ac57aa021f77cd57bbefe507c77a16bb1480a7d
briancoco <briannguyencoco@gmail.com>
2025-01-26 11:19:50 -0600

    link to homepage

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import { FaUserAlt } from "react-icons/fa";
 
-const Layout = ({ userLoggedIn }) => {
+const Layout = () => {
   return (
     <div className="Layout">
       <div className="header">
-        <div className="header-title">-MumboJumbo.</div>
+        <Link to="/" style={{textDecoration: 'none', color: '#DDD0B7'}}><div className="header-title">-MumboJumbo.</div></Link>
         <section className="header-items">
           <div className="header-item">
             <FaUserAlt size={25} />
